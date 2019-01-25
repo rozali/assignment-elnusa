8b0582dd8bf564255cc3227364bff28eb7aabb4a
rozali <rozali.putra@gmail.com>
2019-01-25 22:29:04 +0700

    Adding Function and minor rework

--- register.php
+++ register.php
@@ -1,8 +1,44 @@
 <!-- Registration Method -->
 
 <?php
+    $output = "";
+    $output2 = "";
+    $daftar = TRUE;
     if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+      $username = $_POST['username'];
+      $nama = $_POST['nama'];
+      $email = $_POST['email'];
+      $password1 = $_POST['password1'];
+      $password2 = $_POST['password2'];
+      $register = $_POST['register'];
+      $usernamelength = strlen($username);
       
+      if (isset($register)){
+        if ($usernamelength < 5){
+            $output= "<p class='text-danger'>Invalid username. Username harus 4 karakter atau lebih</p>";
+            $daftar = FALSE;
+        }
+      }
+
+      if (isset($register)){
+        if ($password1 != $password2){
+            $output2= "<p class='text-danger'>Password anda tidak sama</p>";
+            $daftar = FALSE;
+        }
+      }
+
+      if ($daftar) {
+        if ($fh = fopen('user.txt', 'a')) {
+          $input = "\r\n" .$username. "|" .$nama. "|" .$email. "|" .$password2;
+          fwrite($fh, $input);
+          fclose($fh);
+          echo '<script language="javascript">';
+          echo 'alert("Berhasil registrasi. Silahkan Login")';
+          echo '</script>';
+          header("location:login.php");
+        }
+      }
     }
 ?>
 
@@ -25,29 +61,34 @@
         <div class="row justify-content-md-center">
           <div class="col-md-8 order-md-1">
             <h4 class="mb-3">Register to login</h4>
-            <form class="needs-validation" novalidate>
+            <form class="needs-validation" method="POST">
               <div class="mb-3">
                 <label for="username">Username*</label>
-                <div class="input-group">
-                  <input type="text" class="form-control" id="username" placeholder="Username">
-                </div>
+                <input class="form-control" id="username" placeholder="Username" name="username" required>
+                <?php echo $output; ?>
+              </div>
+              <div class="mb-3">
+                <label for="nama">Nama*</label>
+                <input class="form-control" id="nama" placeholder="Nama" name="nama" required>
               </div>
               <div class="row">
                 <div class="col-md-6 mb-3">
                   <label for="firstName">Password*</label>
-                  <input type="password" class="form-control" id="firstName" placeholder="" value="" required>
+                  <input type="password" class="form-control" id="firstName" placeholder="Password" value="" name="password1" required>
+                  <br><?php echo $output2; ?>
                 </div>
                 <div class="col-md-6 mb-3">
                   <label for="lastName">Password Confirm*</label>
-                  <input type="password" class="form-control" id="lastName" placeholder="" value="" required>
+                  <input type="password" class="form-control" id="lastName" placeholder="Password Confirm" value="" name="password2" required>
                 </div>
               </div>
               <div class="mb-3">
-                <label for="email">Email*</label>
-                <input type="email" class="form-control" id="email" placeholder="you@example.com">
+                <label for="inputEmail">Email address*</label>
+                <input type="email" id="inputEmail" class="form-control" placeholder="Email address" name="email" required>
               </div>
+              <p> * Required </p>
               <hr class="mb-4">
-              <button class="btn btn-primary btn-lg btn-block" type="submit">Register</button>
+              <button class="btn btn-primary btn-lg btn-block" type="submit" name="register">Register</button>
             </form>
           </div>
         </div>
